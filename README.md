1f1e567cfed340b3cf240945146123e9a25c4a0d
ktoksoz <ktoksoz@gmail.com>
2018-10-30 19:22:26 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CardReaderInterface
+Card Reader CVS File Viewer, a very simple PoC project
+
+This project is not intended to be used on production environments.
+
---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import csv
+__author__='Koray Toksoz'
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+
+    with open('reader.csv', 'rb') as csvfile:
+        csvReader = csv.reader(csvfile, delimiter=',')
+        cardInfo = list(csvReader)
+
+    return render_template('index.html', cards=cardInfo)
+
+
+if __name__ == '__main__':
+    app.run()
